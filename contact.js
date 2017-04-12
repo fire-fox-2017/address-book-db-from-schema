5fe6f7a09663a4d5886d69768b6690e8e5bf5841
LeCodeBae <billivank_96@yahoo.co.id>
2017-04-12 11:21:39 +0700

    beres

--- contact.js
+++ contact.js
@@ -7,9 +7,9 @@ var db = new sqlite.Database(file);
 class Contact {
   constructor(obj) {
     this.name = obj.name
-    this.company = obj.company
-    this.phone_number = obj.phone_number
-    this.email = obj.email
+    this.company = obj.company || null
+    this.phone_number = obj.phone_number || null
+    this.email = obj.email || null
     this.id = null
   }
   save() {
@@ -19,50 +19,88 @@ class Contact {
     phone_number = this.phone_number,
     email = this.email,
     obj = this;
-    if(id===null){
-      db.serialize(function(){
-        db.run(`INSERT INTO contacts (name, company, phone_number, email) VALUES ('${name}', '${company}', '${phone_number}', '${email}');`,function(err){
-          if(err){
-            console.log(err.message)
-          } else {
-            obj.id = this.lastID
-            console.log(`${name} inserted`)
-          }
-        })
-      })
+    if(Contact.emailValidation(email) || email===null){
+      if(Contact.phoneValidation(phone_number) || phone_number === null){
+        if(id===null){
+          db.serialize(function(){
+            db.run(`INSERT INTO contacts (name, company, phone_number, email) VALUES ('${name}', '${company}', '${phone_number}', '${email}');`,function(err){
+              if(err){
+                console.log(err.message)
+              } else {
+                obj.id = this.lastID
+                console.log(`${name} inserted`)
+              }
+            })
+          })
+        } else {
+          db.serialize(function(){
+            db.run(`UPDATE contacts SET name = '${name}', company = '${company}', phone_number = '${phone_number}', email = '${email}' WHERE id = ${id};`, function(err){
+              if(err){
+                console.log(err.message)
+              } else {
+                console.log(`id ${id} updated`)
+              }
+            })
+          })
+        }
+      } else {
+        console.log("phone number is not valid")
+      }
     } else {
-      db.serialize(function(){
-        db.run(`UPDATE contacts SET name = '${name}', company = '${company}', phone_number = '${phone_number}', email = '${email}' WHERE id = ${id};`, function(err){
-          if(err){
-            console.log(err.message)
-          } else {
-            console.log(`id ${id} updated`)
-          }
-        })
-      })
+      console.log("email is not valid")
     }
+    
   }
   static create(name, company, phone_number, email){
-    db.serialize(function() {
-     db.run(`INSERT INTO contacts (name, company, phone_number, email) VALUES ('${name}', '${company}', '${phone_number}', '${email}');`, function(err) {
-       if (err) {
-         console.log(err.message);
-       } else {
-         console.log('CONTACT ADDED');
-       }
-     });
-   });
+    if(Contact.emailValidation(email)){
+      if(Contact.phoneValidation(phone_number)){
+        db.serialize(function() {
+          db.run(`INSERT INTO contacts (name, company, phone_number, email) VALUES ('${name}', '${company}', '${phone_number}', '${email}');`, function(err) {
+            if (err) {
+              console.log(err.message);
+            } else {
+              console.log('CONTACT ADDED');
+            }
+          });
+        });
+      } else {
+        console.log("phone number is not valid")
+      }
+    } else {
+      console.log("email is not valid")
+    }
   }
   static update(id, attribute, value){
-    db.serialize(function() {
-      db.run(`UPDATE contacts SET ${attribute} = '${value}' WHERE id = ${id};`, function(err) {
-        if (err) {
-          console.log(err.message);
-        } else {
-          console.log('CONTACT UPDATED');
-        }
-      });
-    });
+    if(attribute === "email"){
+      if(Contact.emailValidation(value)){
+        db.serialize(function() {
+          db.run(`UPDATE contacts SET ${attribute} = '${value}' WHERE id = ${id};`, function(err) {
+            if (err) {
+              console.log(err.message);
+            } else {
+              console.log('CONTACT UPDATED');
+            }
+          });
+        });
+      } else {
+        console.log("email is not valid!");
+      }
+    }
+    if(attribute === "phone_number"){
+      if(Contact.phoneValidation(value)){
+        db.serialize(function() {
+          db.run(`UPDATE contacts SET ${attribute} = '${value}' WHERE id = ${id};`, function(err) {
+            if (err) {
+              console.log(err.message);
+            } else {
+              console.log('CONTACT UPDATED');
+            }
+          });
+        });
+      } else {
+        console.log("phone is not valid!");
+      }
+    } 
   }
   static delete(id){
     db.serialize(function() {
@@ -101,7 +139,15 @@ class Contact {
         }
       })
   }
-  
+  static emailValidation(email){
+    let emailregex=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
+    return emailregex.test(email)
+  }
+  static phoneValidation(phone_number){
+    if(phone_number.length < 17){
+     return true  
+    }
+  }
 }
 
 export default Contact;
